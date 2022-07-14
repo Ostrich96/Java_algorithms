309ca883b1e7ea468551400d4ec6c881d9d58bdb
Ostrich96 <liiichen96@163.com>
2022-07-14 00:43:44 -0400

    init java

---
+++ HelloWorld.java
@@ -0,0 +1,10 @@
+public class HelloWorld{
+        public static void main(String[] atgs){
+            System.out.println("hello world");
+    }
+}
+/*
+ * all code in java mast be part of a class.
+ * we delimit the begining and end of segments of code using{and}
+ * 
+ */
